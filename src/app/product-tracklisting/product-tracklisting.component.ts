5fa3d7f8140bc7ecc679b5f60b1864945a1a6d0f
John ayinde <lolaayinde@gmail.com>
2020-09-26 16:21:23 +0100

    Completed module 5 refactor

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -14,6 +14,6 @@ export class ProductTracklistingComponent implements OnInit {
   ngOnInit() {
     this._productService
       .getAlbum(1)
-      .subscribe((response) => (this.albumInfo = response));
+      .subscribe(response => this.albumInfo = response);
   }
 }
